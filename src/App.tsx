5ec05f55c35ef9e50601670789ae1f298e0be1ba
SAURON22 <pedro6@illinois.edu>
2025-10-04 04:24:57 +0200

    first version, set up of links, needs to build the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './icono.png';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  RouteMatch,
+  useParams,
+  BrowserRouter
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  return (<div className='App'>
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> esto es una prueba.
+          Dungeons and Dragons monster search tool
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+    <div className='content'>
+    <BrowserRouter>
+        
+          <nav className='navbar' >
+            <Link className='navbutton' to ="/search">Search</Link>
+            <Link className='navbutton' to ="/galery">Galery</Link>
+          </nav>
+          <Routes>
+            <Route path= "/search" element = {<Search />}/>
+            <Route path='/galery' element = {<Galery/>}/>
+          </Routes>
+    </BrowserRouter>
     </div>
+    </div>
+
   );
+}
+function Search (){
+  return <h1>SEARCH</h1>;
+}
+function Galery (){
+  return <h1>GALERY</h1>;
 }
 
 export default App;
