557432311ff5129f8a3f01ecf62b9ec7584535e6
SAURON22 <pedro6@illinois.edu>
2025-10-06 04:56:54 +0200

    Third version, fully functional search page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './icono.png';
 import './App.css';
-import {useEffect, useState } from 'react';
+import {useEffect, useState, useRef } from 'react';
 import {
   Routes,
   Route,
@@ -41,12 +41,16 @@ function App() {
   );
 }
 
-
-function Search ({listOfMonsters}:any){
-  const [monsterList, setMonsterList]:any = useState([]);
-  const [nextPage, setNextPage]:any = useState("https://api.open5e.com/monsters/?document__slug__iexact=wotc-srd");
-  const [displayList, setDisplayList]:any = useState(<h1>Loading...</h1>);
-  
+//Search Component
+function Search (){
+  const [monsterList, setMonsterList] = useState([]);
+  const [nextPage, setNextPage] = useState("https://api.open5e.com/monsters/?document__slug__iexact=wotc-srd");
+  const [displayList, setDisplayList] = useState(<h1>Loading...</h1>);
+  const [filterText, setFilterText] = useState('');
+  const [order, setOrder] = useState(true); //if order === true ascending else descending.
+  const [sort, setSort] = useState(true); //if sort === true use names, else use Challenge Rating.
+  const first_render = useRef(true);
+  //Chaching the API 
   useEffect(() => {
     if (nextPage){
           axios
@@ -58,27 +62,112 @@ function Search ({listOfMonsters}:any){
       setNextPage(page.data.next);
     })
     .catch(error => console.log(error))
-    }else{
-    const renderedList = monsterList.map((monster:any) => {
-          return <li key={monster.slug}>{monster.name} Cr:{monster.cr}</li>;});
-      setDisplayList(<ul>
-      {renderedList}
-    </ul>);
     }
   //Adding the disable-next-line because it doesn't need to run when monsterList is changed, this could affect the rendering of the list.
 
   // eslint-disable-next-line
   }, [nextPage]);
-
+  //Rendering the list 
+  useEffect(() =>{
+    if (first_render.current === true){
+      first_render.current = false;
+    }else{
+       setDisplayList(<SearchTable monsters = {monsterList} filterText= {filterText} order = {order} sort = {sort}/>);
+    }
+       
+  },[monsterList,filterText, order, sort] )
 
     return (<>
     <h1>SEARCH</h1>
-      {displayList}
+    <SearchBar filterText = {filterText} onFilterTextChange = {setFilterText} 
+    order = {order} onOrderChange = {setOrder}
+    onSortChange ={setSort}/>
+    <div className='SearchTable'>
+    {displayList}
+    </div>
     </>
     )
 }
+function SearchTable({monsters, filterText, order, sort}:any){
+  const [filteredMonsters, setFilterMonsters] = useState([]);
+  useEffect(()=>{
+    const sortedMonsters = monsters;
+    if (sort === true){
+      if (order === true){
+        sortedMonsters.sort((a:any, b:any) => a.slug.localeCompare(b.slug));
+    }else{
+        sortedMonsters.sort((a:any, b:any) => -a.slug.localeCompare(b.slug));
+    }
+    }else{if (order === true){
+        sortedMonsters.sort((a:any, b:any) =>a.cr - b.cr);
+      }else{
+        sortedMonsters.sort((a:any, b:any) =>b.cr - a.cr);
+      }
+    }
+    
+
+    const temp:any = [];
+    sortedMonsters.forEach((monster:any) => {
+    if (monster.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1){
+        return;
+      }
+    else{
+        temp.push(<SearchRow slug = {monster.slug} name = {monster.name} challenge_rating = {monster.cr}/>);
+    }
+  });
+  setFilterMonsters(temp);
+  }, [filterText, monsters, order, sort]);
+  return(
+    <ul>
+      {filteredMonsters}
+    </ul>
+  )
+}
 
 
+function SearchBar({filterText,onFilterTextChange, order, onOrderChange, onSortChange}:any){
+  const [openSelector, setOpenSelector] = useState(false);
+  return(<>
+    <form className='searchBar'>
+      <input 
+        type="text" 
+        value={filterText} placeholder="Search..." 
+        onChange={(e) => onFilterTextChange(e.target.value)} />
+        <div className='orderSearchBar'>
+          <label htmlFor='ascending'>Ascending</label>
+          <input type='radio' id="ascending" name="order" value="ascending" checked = {true === order} onChange={() => onOrderChange(true)}/>
+          <label htmlFor='descending'>Descending</label>
+          <input type='radio' id="descending" name="order" value="descending"checked = {false === order} onChange={() => onOrderChange(false)}/>
+        </div>
+    </form> 
+    <div className="dropdown">
+      <button onClick={() => setOpenSelector(true)}>Sort by</button>
+            {openSelector ? (
+        <ul className="menu">
+          <li className="menu-item">
+            <button onClick={() => {setOpenSelector(false);
+                                    onSortChange(true)
+            }
+            }>Names</button>
+          </li>
+          <li className="menu-item">
+            <button onClick={() => {setOpenSelector(false);
+                                    onSortChange(false);
+            }
+            }>Challenge rating</button>
+          </li>
+        </ul>
+      ) : null}
+    </div>
+    </>
+  );
+}
+function SearchRow({slug, name, challenge_rating}:any){
+  return <li key={slug} className='searchRow'><p>{name}</p> <p>Cr:{challenge_rating}</p></li>;
+
+}
 function Galery (){
     const [monsterList, setMonsterList]:any = useState([]);
   const [nextPage, setNextPage]:any = useState("https://api.open5e.com/monsters/?document__slug__iexact=wotc-srd");
