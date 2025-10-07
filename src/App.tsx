f5af2483e32169c0ed68c01f978a082a3dba69c8
SAURON22 <pedro6@illinois.edu>
2025-10-07 04:44:42 +0200

    Forth version, added galery view and begin to create detailed view

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import logo from './icono.png';
 import './App.css';
 import {useEffect, useState, useRef } from 'react';
 import {
   Routes,
   Route,
   Link,
-  BrowserRouter
+  BrowserRouter,
+  useParams
 } from "react-router-dom";
 import axios from 'axios';
-//Personalized instance of axios, with base url and throttle to avoid API rate limits.
-
+//Images used in the app
+import logo from './icono.png';
+import aberration from './aberration.svg';
+import beast from './beast.svg';
+import celestial from './celestial.svg';
+import construct from './construct.svg';
+import dragon from './dragon.svg';
+import elemental from './elemental.svg';
+import fae from './fae.svg';
+import fiend from './fiend.svg';
+import giant from './giant.svg';
+import humanoid from './humanoid.svg';
+import monstrosity from './monstrosity.svg';
+import ooze from './ooze.svg';
+import plant from './plant.svg';
+import undead from './undead.svg';
 
 function App() {
   return (<div className='App'>
@@ -33,6 +47,7 @@ function App() {
             <Route path='/'></Route>
             <Route path= "/search" element = {<Search/>}/>
             <Route path='/galery' element = {<Galery/>}/>
+            <Route path='/:monsterName' element = {<DetailedMonster/>}/>
           </Routes>
     </BrowserRouter>
     </div>
@@ -172,36 +187,134 @@ function Galery (){
     const [monsterList, setMonsterList]:any = useState([]);
   const [nextPage, setNextPage]:any = useState("https://api.open5e.com/monsters/?document__slug__iexact=wotc-srd");
   const [displayList, setDisplayList]:any = useState(<h1>Loading...</h1>);
-  
+  const [filter,setFilter] = useState("all");
+
   useEffect(() => {
     if (nextPage){
           axios
     .get(nextPage)
     .then((page) => {
       const monsters = monsterList.concat(page.data.results);
-      console.log(monsterList);
-      console.log(page.data.results);
       setMonsterList(monsters);
       setNextPage(page.data.next);
     })
     .catch(error => console.log(error))
-    }else{
-    console.log("End");
-    console.log(monsterList);
-    const renderedList = monsterList.map((monster:any) => {
-          return <img src ={monster.img_main} alt = {monster.name}/>;});
-      setDisplayList(renderedList);
     }
   //Adding the disable-next-line because it doesn't need to run when monsterList is changed, this could affect the rendering of the list.
 
   // eslint-disable-next-line
   }, [nextPage]);
-  return (<>
+  useEffect (()=>{
+    const monsterTypes = [
+        {"type": "aberration", "file": aberration},
+        {"type": "beast", "file": beast},
+        {"type": "celestial", "file": celestial},
+        {"type": "construct", "file": construct},
+        {"type": "dragon", "file": dragon},
+        {"type": "elemental", "file": elemental},
+        {"type": "fae", "file":fae},
+        {"type": "fiend", "file":fiend},
+        {"type": "giant", "file": giant},
+        {"type": "humanoid", "file": humanoid},
+        {"type": "monstrosity", "file": monstrosity},
+        {"type": "plant", "file": plant},
+        {"type": "ooze", "file": ooze},
+        {"type":"undead", "file":undead}
+      ];
+    // eslint-disable-next-line
+    const renderedList = monsterList.map((monster:any) => {
+      if (filter === "all"){
+        if (monster.img_main === null){
+        const lowerCaseMonsterType = monster.type.toLowerCase();
+      //Not using for each to end execution when desired result is achived
+      for (let i = 0; i < monsterTypes.length; i++){
+        if (monsterTypes[i].type === lowerCaseMonsterType){
+          console.log("Hey");
+          return (<div className='monsterImagediv'>
+                <img className='monsterImageimg' src ={monsterTypes[i].file} alt={monster.name}/>
+                <p>{monster.name}</p>
+              </div>)
+        }
+      }
+      }else{
+      return (<div className='monsterImagediv'>
+            <img className='monsterImageimg' src ={monster.img_main} alt = {monster.name}/>
+          </div>)
+      }
+      }else{
+        if (monster.type.toLowerCase() === filter){
+          if (monster.img_main === null){
+            const lowerCaseMonsterType = monster.type.toLowerCase();
+      //Not using for each to end execution when desired result is achived
+            for (let i = 0; i < monsterTypes.length; i++){
+              if (monsterTypes[i].type === lowerCaseMonsterType){
+                console.log("Hey");
+                return (<div className='monsterImagediv'>
+                      <img className='monsterImageimg' src ={monsterTypes[i].file} alt={monster.name}/>
+                      <p>{monster.name}</p>
+                    </div>)
+              }
+            }
+            }else{
+            return (<div className='monsterImagediv'>
+                  <img className='monsterImageimg' src ={monster.img_main} alt={monster.name}/>
+                </div>)
+            }
+        }
+      }
+          });
+      setDisplayList(renderedList);
+  }, [filter,monsterList]);
+  return (<div className='Galery'>
   <h1>GALERY</h1>
-  <div>
+  <GaleryFilter filter = {filter} onChangeFilter = {setFilter}/>
+  <div className='galeryDisplay'>
     {displayList}
   </div>
-  </>)
+  </div>)
   
+}
+
+function GaleryFilter({filter, onChangeFilter}:any){
+  return(<div className='galeryFilter'>
+    <form>
+    <input type= "radio" id = "all" name= "filter" value= "all" checked = {filter === "all"} onChange={() => onChangeFilter("all")}/>
+    <label htmlFor = "all">All</label>
+    <input type= "radio" id = "aberration" name= "filter" value= "aberration"checked = {filter === "aberration"} onChange={() => onChangeFilter("aberration")}/>
+    <label htmlFor = "aberration">Aberration</label>
+    <input type= "radio" id = "beast" name= "filter" value= "beast"checked = {filter === "beast"} onChange={() => onChangeFilter("beast")}/>
+    <label htmlFor = "beast">Beast</label>
+    <input type= "radio" id = "celestial" name= "filter" value= "celestial" checked = {filter === "celestial"} onChange={() => onChangeFilter("celestial")}/>
+    <label htmlFor = "celestial">Celestial</label>
+    <input type= "radio" id = "construct" name= "filter" value= "construct" checked = {filter === "construct"} onChange={() => onChangeFilter("construct")}/>
+    <label htmlFor = "construct">Construct</label>
+    <input type= "radio" id = "dragon" name= "filter" value= "dragon"checked = {filter === "dragon"} onChange={() => onChangeFilter("dragon")}/>
+    <label htmlFor = "dragon">Dragon</label>
+    <input type= "radio" id = "elemental" name= "filter" value= "elemental"checked = {filter === "elemental"} onChange={() => onChangeFilter("elemental")}/>
+    <label htmlFor = "elemental">Elemental</label>
+    <input type= "radio" id = "fae" name= "filter" value= "fae" checked = {filter === "fae"} onChange={() => onChangeFilter("fae")}/>
+    <label htmlFor = "fae">Fae</label>
+    <input type= "radio" id = "fiend" name= "filter" value= "fiend" checked = {filter === "fiend"} onChange={() => onChangeFilter("fiend")}/>
+    <label htmlFor = "fiend">Fiend</label>
+    <input type= "radio" id = "giant" name= "filter" value= "giant" checked = {filter === "giant"} onChange={() => onChangeFilter("giant")}/>
+    <label htmlFor = "giant">Giant</label>
+    <input type= "radio" id = "humanoid" name= "filter" value= "humanoid" checked = {filter === "humanoid"} onChange={() => onChangeFilter("humanoid")}/>
+    <label htmlFor = "humanoid">Humanoid</label>
+    <input type= "radio" id = "monstrosity" name= "filter" value= "monstrosity" checked = {filter === "monstrosity"} onChange={() => onChangeFilter("monstrosity")}/>
+    <label htmlFor = "monstrosity">Monstrosity</label>
+    <input type= "radio" id = "ooze" name= "filter" value= "ooze" checked = {filter === "ooze"} onChange={() => onChangeFilter("ooze")}/>
+    <label htmlFor = "ooze">Ooze</label>
+    <input type= "radio" id = "plant" name= "filter" value= "plant" checked = {filter === "plant"} onChange={() => onChangeFilter("plant")}/>
+    <label htmlFor = "plant">Plant</label>
+    <input type= "radio" id = "undead" name= "filter" value= "undead" checked = {filter === "undead"} onChange={() => onChangeFilter("undead")}/>
+    <label htmlFor = "undead">Undead</label>
+
+  </form>
+  </div>)
+}
+
+function DetailedMonster(){
+    const {monsterName} = useParams();
+    return <h1>{monsterName}</h1>
 }
 export default App;
